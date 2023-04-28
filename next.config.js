bf372d5203b188b8c31f48d25c697043461cc35c
ditinex <asifakramsk@gmail.com>
2023-04-28 23:36:14 +0530

    added export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 const nextConfig = {
+  output: "export",
+  distDir: "dist",
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias['@/react-icons'] = path.join(__dirname, 'src', 'react-icons')
-    return config
+    config.resolve.alias["@/react-icons"] = path.join(
+      __dirname,
+      "src",
+      "react-icons"
+    );
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
