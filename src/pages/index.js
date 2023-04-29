c08d2ece64be18e577e1bf1abf38451359303fc1
Raees-Mondal9887 <102875335+Raees-Mondal9887@users.noreply.github.com>
2023-04-29 10:46:26 +0530

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 
-import Body from '@/layout/Body';
+import Body from '@/layout/Home';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <meta charSet="UTF-8" />
       <title>Keyshell</title>
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Keyshell - Empowering Your Business with Innovative IT Solutions" />
       <meta name="keywords" content="Keyshell, IT Solutions, Business, Innovation" />
