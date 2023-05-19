2e83fe2a16987399b059fb2a4426f0c09a903bd0
web-vikas <msg_vikas@yahoo.com>
2023-05-19 10:39:26 +0530

    Update

--- src/layout/Home/index.js
+++ src/layout/Home/index.js
@@ -11,18 +11,30 @@ const index = () => {
         <div className="flex flex-wrap gap-y-0 gap-x-5 items-start md:items-center">
           <div className="flex items-center gap-2">
             <span>
+              <Image
+                src="/images/phone-call.png"
+                alt="call"
+                height={30}
+                width={30}
+              />
               <i className="fa-solid fa-phone-volume pt-[2px] text-primary text-xs md:text-base"></i>
             </span>
-            <p className="font-semibold text-xs md:text-base text-[#333]">
+            <p className="font-semibold text-xs md:text-base text-secondary">
               +91 8867676785
             </p>
           </div>
 
           <div className="flex items-center gap-2">
             <span>
+              <Image
+                src="/images/mail.png"
+                alt="call"
+                height={30}
+                width={30}
+              />
               <i className="fa-solid fa-envelope text-primary text-xs md:text-base"></i>
             </span>
-            <p className="font-semibold text-xs md:text-base text-[#333]">
+            <p className="font-semibold text-xs md:text-base text-secondary">
               keyshell3455@gmail.com
             </p>
           </div>
@@ -30,7 +42,7 @@ const index = () => {
       </div>
       <Nav />
       <main className="container mx-auto bg-white">
-        <section className="text-black body-font">
+        {/* <section className="text-black body-font">
           <div className="container mx-auto flex px-5 py-8 md:flex-row flex-col-reverse items-center gap-8">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
@@ -60,7 +72,40 @@ const index = () => {
               />
             </div>
           </div>
-        </section>
+        </section> */}
+        <div className="text-black p-4 md:p-0">
+          <h1 className=" text-5xl lg:text-[7rem] leading-none font-extrabold mt-9 ">
+            <span className="text-primary">Empowering</span> Your Business
+            with Innovative IT Solutions
+          </h1>
+          <div className="flex justify-between py-11 flex-col lg:flex-row gap-5 lg:gap-0">
+            <p className="text-lg font-semibold max-w-2xl">
+              From custom software development to cloud computing and
+              cybersecurity solutions, we offer a wide range of services that
+              are tailored to meet the unique needs of each of our clients.
+              We've got you covered!
+            </p>
+            <div className="">
+              <button className="rounded-lg bg-primary p-3 px-5 font-bold text-lg text-white flex justify-center items-center gap-4  shadow-md hover:shadow-2xl transition-all">
+                Get a <br />
+                Quote
+                <svg
+                  className="animate-[spin_2s_linear_infinite]"
+                  width="42"
+                  height="39"
+                  viewBox="0 0 42 39"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M19.7572 1.57911C19.9824 0.52425 21.4878 0.524246 21.713 1.5791L23.7901 11.3047C23.9337 11.9771 24.6974 12.3073 25.2857 11.9514L34.3928 6.4418C35.3387 5.86956 36.3767 7.02861 35.704 7.9059L29.83 15.5664C29.3714 16.1646 29.7142 17.0358 30.4574 17.161L40.6181 18.8731C41.7298 19.0605 41.7298 20.658 40.6181 20.8453L30.4574 22.5574C29.7142 22.6826 29.3714 23.5539 29.83 24.152L35.704 31.8125C36.3766 32.6898 35.3387 33.8489 34.3928 33.2766L25.2857 27.767C24.6974 27.4111 23.9337 27.7413 23.7901 28.4137L21.713 38.1393C21.4878 39.1942 19.9824 39.1942 19.7572 38.1393L17.6801 28.4137C17.5365 27.7413 16.7728 27.4111 16.1845 27.767L7.07743 33.2766C6.13154 33.8489 5.09355 32.6898 5.76624 31.8125L11.6402 24.152C12.0988 23.5539 11.756 22.6826 11.0128 22.5574L0.852148 20.8453C-0.259639 20.658 -0.259639 19.0605 0.852148 18.8731L11.0128 17.161C11.756 17.0358 12.0988 16.1646 11.6402 15.5664L5.76625 7.9059C5.09355 7.02861 6.13155 5.86956 7.07743 6.44181L16.1845 11.9514C16.7728 12.3073 17.5365 11.9771 17.6801 11.3047L19.7572 1.57911Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
             <div className="text-center mb-20">
