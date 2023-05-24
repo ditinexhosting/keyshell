f23b1bc2a5b843b7f4a1724467452b425dfc81bf
web-vikas <msg_vikas@yahoo.com>
2023-05-24 10:19:27 +0530

    WEb DEv page added

--- src/pages/campaign/web-development/index.js
+++ src/pages/campaign/web-development/index.js
@@ -8,8 +8,10 @@ const index = () => {
     <>
       <Nav />
       <main className="container mx-auto my-10 text-secondary px-5">
-        <h1 className="text-4xl md:text-7xl font-bold">Web Development</h1>
-        <div className="flex items-center gap-2 justify-start my-5">
+        <h1 className="text-4xl md:text-7xl font-bold md:mb-10 mb-5">
+          Introducing our Extraordinary Website Experience
+        </h1>
+        {/* <div className="flex items-center gap-2 justify-start my-5">
           <div className="rounded-full border-2 text-sm p-2 px-4 shrink-0">
             Android
           </div>
@@ -20,72 +22,104 @@ const index = () => {
             Iso
           </div>
           <div className="w-full h-[2px] bg-gray-400 hidden md:block mx-2"></div>
-        </div>
+        </div> */}
         <div className="flex items-start  mx-auto  sm:flex-row flex-col">
           <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
             <div className="flex items-baseline justify-between mb-5">
               <h2 className="text-primary text-2xl title-font font-bold mb-5 text-justify">
-                The Problem
+                Cutting-Edge Web Solutions
               </h2>
               <button className="hover:text-primary hover:bg-white font-bold text-md border-2 border-primary rounded-full px-3 py-2 text-white bg-primary pointer transition-colors">
                 Book a Call
               </button>
             </div>
             <p className="leading-relaxed text-base text-justify">
-              In today's fast-paced business environment, keeping your employees
-              up-to-date with the latest knowledge and skills is crucial to
-              maintaining a competitive edge. That's why at Keyshell, we offer a
-              mobile learning app solution that empowers your workforce with
-              anytime, anywhere access to learning materials. <br />
-              <br />
-              Our mobile learning app solution is customized to meet the
-              specific needs of your business and industry. Our team of
-              experienced developers and instructional designers work closely
-              with you to create engaging and interactive content that aligns
-              with your learning objectives.
+              At our cutting-edge website, we excel in providing unmatched web
+              application development services.
+              <br /> <br />
+              Our team of highly skilled professionals, Project managers,
+              developers, UX designers,quality tester,analysts who collaborates
+              seamlessly to deliver exceptional web app development solutions.
+              We are dedicated to surpassing your expectations and providing you
+              with a truly remarkable online experience.
+              <br /> <br />
+              In the ever-evolving field of web application development, we stay
+              ahead of the curve. Our team stays abreast of the latest industry
+              trends and developments, ensuring that our clients receive
+              state-of-the-art solutions. Whether you require web application
+              development, mobile web development, or the expertise of our
+              talented website and app developers, we have the knowledge and
+              experience to meet your specific needs. Our commitment to
+              continuous learning and adoption of the latest technologies
+              guarantees that our clients always receive top-notch solutions
+              tailored to their unique requirements.
             </p>
             <div className="mt-10">
               <h2 className="text-primary text-2xl title-font font-bold mb-5 text-justify">
-                The Solution
+                The Process of Building Your Dream Website
               </h2>
-              <p className="leading-relaxed text-base text-justify">
+              {/* <p className="leading-relaxed text-base text-justify">
                 Some of the key features of our mobile learning app solution
                 include:
-              </p>
+              </p> */}
               <ul>
                 <li className="my-5 text-justify">
                   <h3 className="text-primary font-bold text-md inline text-justify mr-2">
-                    Customized Content:
+                    Discovery and Planning:
                   </h3>
-                  We create customized content that is tailored to your business
-                  needs and industry-specific requirements. Interactive
-                  Learning: Our mobile learning app features interactive
-                  learning modules, including quizzes, games, and simulations,
-                  to keep learners engaged and motivated.
+                  We work closely with you to understand your goals, conduct a
+                  thorough needs assessment, and create a comprehensive project
+                  plan for custom web application development services.
                 </li>
                 <li className="my-5 text-justify">
                   <h3 className="text-primary font-bold text-md inline text-justify mr-2">
-                    Progress Tracking:
+                    Research and Design:
                   </h3>
-                  Our app allows learners to track their progress and receive
-                  personalized feedback, ensuring that they stay on track and
-                  meet their learning objectives.
+                  Leveraging in-depth industry research, we create visually
+                  stunning designs that are perfectly aligned with your brand
+                  and target audience. Our expertise as the best web and mobile
+                  app development company ensures that your website meets the
+                  highest standards.
                 </li>
                 <li className="my-5 text-justify">
                   <h3 className="text-primary font-bold text-md inline text-justify mr-2">
-                    Offline Access:
+                    Development and Optimization:
                   </h3>
-                  Our mobile learning app allows learners to access learning
-                  materials even without an internet connection, making it
-                  convenient for employees who are always on the go.
+                  Our skilled team utilizes the latest technologies to build a
+                  responsive and optimized website, ensuring seamless
+                  performance across all devices and platforms. We specialize in
+                  developing custom web applications and employ expert app
+                  developers for websites.
                 </li>
                 <li className="my-5 text-justify">
                   <h3 className="text-primary font-bold text-md inline text-justify mr-2">
-                    Analytics and Reporting:
+                    Content Creation and Promotion:
                   </h3>
-                  Our app provides in-depth analytics and reporting features to
-                  help you track learner performance, identify knowledge gaps,
-                  and make data-driven decisions.
+                  We go beyond aesthetics and craft engaging, SEO-optimized
+                  content that captivates your audience. Our team understands
+                  the importance of app design and develops mobile apps that
+                  complement your website, enhancing user experience.
+                  Additionally, we implement effective marketing strategies to
+                  drive traffic and enhance your online visibility.
+                </li>
+                <li className="my-5 text-justify">
+                  <h3 className="text-primary font-bold text-md inline text-justify mr-2">
+                    Quality Assurance and Launch:
+                  </h3>
+                  Prior to the launch, we perform rigorous testing to ensure a
+                  bug-free, smooth website experience. Our meticulous attention
+                  to detail guarantees a flawless launch for your web and mobile
+                  applications.
+                </li>
+                <li className="my-5 text-justify">
+                  <h3 className="text-primary font-bold text-md inline text-justify mr-2">
+                    Training and Ongoing Support:
+                  </h3>
+                  We provide comprehensive training, ongoing maintenance, and
+                  continuous support to keep your website, web applications, and
+                  mobile apps up-to-date and running smoothly. As a leading app
+                  developer and web design company, our team is always available
+                  to assist you with any queries or updates.
                 </li>
               </ul>
               <p className="text-justify">
